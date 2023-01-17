2ef406725eb9a913e19435ebac872f9ff39161bf
Elliot <elliotcs30@gmail.com>
2023-01-18 00:00:54 +0800

    fix: eslint setting

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"commonjs": true,
+		"es6": true, 	// add
+		"node": true, // add
+		"es2021": true
+	},
+	"extends": "eslint:recommended",
+	"overrides": [
+	],
+	"parserOptions": {
+		"ecmaVersion": "latest"
+	},
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"never"
+		]
+	}
+}
