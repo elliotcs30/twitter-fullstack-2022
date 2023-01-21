390400d2344aa339fac5a230c84a48612c2e77a6
Elliot <elliotcs30@gmail.com>
2023-01-21 19:12:26 +0800

    feat: add user model and association

--- models/user.js
+++ models/user.js
@@ -1,9 +1,45 @@
 'use strict'
+const { Model } = require('sequelize')
+
 module.exports = (sequelize, DataTypes) => {
 	// 定義 User 的資料結構
-	const User = sequelize.define('User', {
-	}, {})
-	User.associate = function(models) {
+	class User extends Model {
+		static associate (models) {
+			// define association here
+			User.hasMany(models.Reply, { foreignKey: 'UserId' })
+			User.hasMany(models.Tweet, { foreignKey: 'UserId' })
+			User.hasMany(models.Like, { foreignKey: 'UserId' })
+
+			User.belongsToMany(User, {
+				through: models.Followship,
+				foreignKey: 'followingId',
+				as: 'Followers'
+			})
+
+			User.belongsToMany(User, {
+				through: models.Followship,
+				foreignKey: 'followerId',
+				as: 'Followings'
+			})
+		}
 	}
+
+	User.init({
+		email: DataTypes.STRING,
+		password: DataTypes.STRING,
+		name: DataTypes.STRING,
+		avatar: DataTypes.STRING,
+		introduction: DataTypes.Text,
+		role: DataTypes.STRING,
+		account: DataTypes.STRING,
+		cover: DataTypes.STRING,
+		createdAt: DataTypes.DATE,
+		updatedAt: DataTypes.DATE
+	}, {
+		sequelize,
+		modelName: 'User',
+		tableName: 'Users',
+		underscored: true
+	})
 	return User
 }
