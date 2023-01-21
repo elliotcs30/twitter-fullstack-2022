9738309183c1354068136bb44e937e88dc95762d
Elliot <elliotcs30@gmail.com>
2023-01-21 20:44:51 +0800

    feat: add tweet model and association

--- models/tweet.js
+++ models/tweet.js
@@ -1,8 +1,24 @@
 'use strict'
+const { Model } = require('sequelize')
+
 module.exports = (sequelize, DataTypes) => {
-	const Tweet = sequelize.define('Tweet', {
-	}, {})
-	Tweet.associate = function(models) {
+	class Tweet extends Model {
+		static associate (models) {
+			// define association here
+			Tweet.hasMany(models.Reply, { foreignKey: 'TweetId'})
+			Tweet.hasMany(models.Like, { foreignKey: 'TweetId'})
+			Tweet.belongsTo(models.User, { foreignKey: 'UserId'})
+		}
 	}
+
+	Tweet.init({
+		UserId: DataTypes.INTEGER,
+		description: DataTypes.Text
+	}, {
+		sequelize,
+		modelName: 'Tweet',
+		tableName: 'Tweets',
+		underscored: true
+	})
 	return Tweet
 }
