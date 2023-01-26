a642d076344aa5fa31722c1c3182f555789f0463
Elliot <elliotcs30@gmail.com>
2023-01-26 18:50:54 +0800

    modify: annotation and format

--- seeders/20230123064730-users-seed-file.js
+++ seeders/20230123064730-users-seed-file.js
@@ -11,7 +11,7 @@ module.exports = {
 			account: `user${i + 1}`,
 			name: `user${i + 1}`,
 			email: `user${i + 1}@example.com`,
-			password:  bcrypt.hashSync(password, 10),
+			password:	bcrypt.hashSync(password, 10),
 			role: 'user',
 			avatar: `https://loremflickr.com/320/240/man,woman/?random=${Math.floor(Math.random() * 50)}`,
 			introduction: faker.lorem.sentence(3),
@@ -19,7 +19,7 @@ module.exports = {
 			updated_at: new Date()
 		}))
 		
-		// 一次新增三筆資料
+		// 一次新增 10 筆資料
 		await queryInterface.bulkInsert('Users', [
 			{
 				account: 'root',
