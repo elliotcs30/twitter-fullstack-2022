c5bd527af5d31c22aae40a864f793c9e1b264829
Elliot <elliotcs30@gmail.com>
2023-02-02 22:37:17 +0800

    feat: add use helpers getUser and dayjs method

--- _helpers.js
+++ _helpers.js
@@ -1,12 +1,15 @@
+const dayjs = require('dayjs')
+
 function ensureAuthenticated(req) {
 	return req.isAuthenticated()
 }
 
 function getUser(req) {
-	return req.user
+	return req.user || null
 }
 
 module.exports = {
+	currentYear: () => dayjs().year(),
 	ensureAuthenticated,
 	getUser,
 }
--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport')
-const routes = require('./routes')
+const handlebarsHelpers = require('./_helpers')
 const helpers = require('./_helpers')
+const routes = require('./routes')
+
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
 
-app.engine('hbs', handlebars({ extname: '.hbs' }))
+app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
 app.set('view engine', 'hbs')
 app.use(express.static('public')) //匯入靜態檔案
 app.use(express.urlencoded({ extended: true }))
@@ -20,12 +22,10 @@ app.use(flash())
 app.use((req, res, next) => {
 	res.locals.success_message = req.flash('success_messages')
 	res.locals.error_messages = req.flash('error_messages')
+	res.locals.user = helpers.getUser(req)
 	next()
 })
 app.use(routes)
-
-// use helpers.getUser(req) to replace req.user
-// use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
