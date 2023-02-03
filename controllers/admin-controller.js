673ee048268a78fa63c337b19fabb38a0088d7c0
Elliot <elliotcs30@gmail.com>
2023-02-03 14:19:00 +0800

    feat: add admin signin page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,22 @@
+const { getUser } = require('../_helpers')
+
 const adminController = {
+	signInPage: (req, res) => {
+		return res.render('admin/signin')
+	},
+	signIn: (req, res) => {
+		if (getUser(req).role === 'user') {
+			req.flash('error_messages', '請前往前台登入')
+			return res.redirect('/admin/signin')
+		}
+		req.flash('success_messages', '登入成功！')
+		return res.redirect('/admin/tweets')
+	},
+	logout: (req, res) => {
+		req.flash('success_messages', '登出成功！')
+		req.logout()
+		return res.redirect('/admin/signin')
+	},
 	getTweets: (req, res) => {
 		return res.render('admin/tweets')
 	}
